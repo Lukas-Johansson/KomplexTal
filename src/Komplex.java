480abaf3e5e3d28085d9e5af5a5a27952f4b054c
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-03-16 13:36:32 +0100

    Initial commit

---
+++ src/Komplex.java
@@ -0,0 +1,13 @@
+public class Komplex {
+
+    public Komplex(double v, double v1) {
+    }
+
+    public Komplex add(Komplex b) {
+
+        return b;
+    }
+
+    public Komplex multiply(Komplex b) {
+    }
+}
