8d06df7b905a619a364d3a8ac4fdc143ca7a485e
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-03-22 15:09:53 +0100

    Mer saker

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,13 +1,30 @@
 public class Komplex {
+    double a = 0;
+    double b = 0;
 
-    public Komplex(double v, double v1) {
+
+    public Komplex(double re, double im) {
+
+        this.a = re;
+        this.b = im;
     }
 
-    public Komplex add(Komplex b) {
-
-        return b;
+    public Komplex add(Komplex add) {
+        add.a = a + add.a;
+        add.b = b + add.b;
+        return add;
     }
 
     public Komplex multiply(Komplex b) {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "Komplex{" + a + " " + "+" + " " + b + "i" + "}";
+    }
+
+    public double arg() {
+        return a + b;
     }
 }
